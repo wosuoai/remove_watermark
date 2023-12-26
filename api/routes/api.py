0aae29fcef343042630545da23e1e298f6716b87
wosuoai <yuchajia@gmail.com>
2023-12-26 19:54:59 +0800

    init: B站路由注册

--- api/routes/api.py
+++ api/routes/api.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 import app.app_douyin.douyin_api as douyin # 注册抖音图文或视频解析路由
 import app.app_redbook.redbook_api as redbook # 注册小红书图文或视频解析路由
 import app.app_kuaishou.kuaishou_api as kuaishou # 注册快手图文或视频解析路由
+import app.app_bilibili.bilibili_api as bilibili # 注册B站图文或视频解析路由
 import app.baseAPI as baseAPI
 router = APIRouter()
 
@@ -10,3 +11,4 @@ router.include_router(baseAPI.router, tags=["健康检查"]) # 注册抖音视�
 router.include_router(douyin.router, tags=["抖音图文或视频链接提取"], prefix="/douyin") # 注册抖音图文或视频链接
 router.include_router(redbook.router, tags=["小红书图文或视频链接提取"], prefix="/redbook") # 注册小红书图文或视频链接
 router.include_router(kuaishou.router, tags=["快手图文或视频链接提取"], prefix="/kuaishou") # 注册快手图文或视频链接
+router.include_router(bilibili.router, tags=["快手图文或视频链接提取"], prefix="/bilibili") # 注册B站图文或视频链接
